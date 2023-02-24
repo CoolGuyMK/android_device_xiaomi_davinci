d4d1b6898ef3cbca4f89a8a7f56832984a66cd3f
CoolGuyMK <coolguymk1@gmail.com>
2023-02-24 17:08:13 +0000

    davinci: adapt for SuperiorOS

--- superior_davinci.mk
+++ superior_davinci.mk
@@ -11,10 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := lineage_davinci
+TARGET_BOOT_ANIMATION_RES := 1080
+
+MAINTAINER := MKツ
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+SUPERIOR_UDFPS_ANIMATIONS := true
+
+PRODUCT_NAME := superior_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
